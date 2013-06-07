720dadefcd071b1711863b72bd7984b2f72e62e3
Steve Tooke <steve@heavi.es>
2013-06-07 08:02:27 +0100

    Create initializer with private attr_readers easily

---
+++ lib/cucumber/initialize.rb
@@ -0,0 +1,18 @@
+module Cucumber
+  def self.Initialize(*attributes)
+    Module.new do
+      attr_reader *attributes
+      private *attributes
+
+      define_method(:initialize) do |*arguments|
+        if attributes.size != arguments.size
+          raise ArgumentError, "wrong number of arguments (#{arguments.size} for #{attributes.size})"
+        end
+
+        attributes.zip(arguments) do |attribute, argument|
+          instance_variable_set("@#{attribute}", argument)
+        end
+      end
+    end
+  end
+end
