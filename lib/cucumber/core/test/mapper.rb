118156b26479da482bbd4ce9b44afe73084d06e1
Matt Wynne <matt@mattwynne.net>
2013-07-15 13:44:32 +0100

    Extract class to reduce imperative code in Mapper

--- lib/cucumber/core/test/mapper.rb
+++ lib/cucumber/core/test/mapper.rb
@@ -6,27 +6,35 @@ module Cucumber
         include Cucumber.initializer(:mappings, :runner)
 
         def test_case(test_case, &descend)
-          @steps= []
+          @steps = []
           descend.call(self)
           new_test_case = test_case.with_steps(@steps)
           new_test_case.describe_to(runner)
         end
 
         def test_step(test_step)
-          @current_test_step = test_step
-          @defined = nil
-          mappings.define(test_step, self)
-          undefined if @defined == nil
+          mapper = StepMapper.new(test_step)
+          mappings.define(test_step, mapper)
+          @steps << mapper.mapped_test_step
         end
 
-        def define(&block)
-          @defined = true
-          @steps << @current_test_step.define(&block)
+        class StepMapper
+          include Cucumber.initializer(:test_step)
+
+          def initialize(*)
+            super
+            @mapped_test_step = nil
+          end
+
+          def define(&block)
+            @mapped_test_step = test_step.define(&block)
+          end
+
+          def mapped_test_step
+            @mapped_test_step || test_step
+          end
         end
 
-        def undefined
-          @steps << @current_test_step
-        end
       end
     end
   end
