ad6f45f8f27f227a4b785cfa6ca1500cdf260db5
Matt Wynne <matt@mattwynne.net>
2013-07-24 11:33:08 +0100

    Refactor #17

--- lib/cucumber/core/test/runner.rb
+++ lib/cucumber/core/test/runner.rb
@@ -7,97 +7,93 @@ module Cucumber
         include Cucumber.initializer(:report)
 
         def test_case(test_case, &descend)
-          case_runner = CaseRunner.new(report)
           report.before_test_case(test_case)
-          descend.call(case_runner)
-          report.after_test_case(test_case, case_runner.result)
+          descend.call
+          report.after_test_case(test_case, current_case_result)
+          @current_case_status = nil
+        end
+
+        def test_step(test_step)
+          report.before_test_step test_step
+          step_result = current_case_status.execute(test_step)
+          report.after_test_step test_step, step_result
         end
 
         private
 
-        class CaseRunner
-          include Cucumber.initializer(:report)
-
-          def test_step(test_step)
-            report.before_test_step test_step
-            result = status.execute(test_step)
-            report.after_test_step test_step, result
-          end
-
-          def result
-            status.result
-          end
-
-          def status
-            @status ||= Status::Monitor.new
-          end
-
-          module Status
-            class Monitor
-              def execute(test_step)
-                status.execute(test_step, self)
-              end
-
-              def result
-                status.result
-              end
-
-              def failed
-                @status = Status::Failed.new
-              end
-
-              def exception(exception)
-              end
-
-              def undefined
-                failed
-              end
-
-              def passed
-                @status = Status::Passing.new
-              end
-
-              def skipped
-              end
-
-              def duration(*)
-              end
-
-              private
-
-              def status
-                @status ||= Status::Unknown.new
-              end
-            end
-
-            Unknown = Class.new do
-              def execute(test_step, monitor)
-                test_step.execute.describe_to(monitor)
-              end
-
-              def result
-                Result::Unknown.new
-              end
-            end
-
-            Passing = Class.new(Unknown) do
-              def result
-                Result::Passed.new
-              end
-            end
-
-            Failed = Class.new do
-              def execute(test_step, monitor)
-                test_step.skip
-              end
-
-              def result
-                Result::Failed.new
-              end
-            end
-          end
-
+        def current_case_result
+          current_case_status.result
         end
+
+        def current_case_status
+          @current_case_status ||= CaseStatus::Monitor.new
+        end
+
+        module CaseStatus
+          class Monitor
+            def execute(test_step)
+              status.execute(test_step, self)
+            end
+
+            def result
+              status.result
+            end
+
+            def failed
+              @status = Failed.new
+            end
+
+            def passed
+              @status = Passing.new
+            end
+
+            def undefined
+              failed
+            end
+
+            def exception(exception)
+            end
+
+            def skipped
+            end
+
+            def duration(*)
+            end
+
+            private
+
+            def status
+              @status ||= Unknown.new
+            end
+          end
+
+          Unknown = Class.new do
+            def execute(test_step, monitor)
+              test_step.execute.describe_to(monitor)
+            end
+
+            def result
+              Result::Unknown.new
+            end
+          end
+
+          Passing = Class.new(Unknown) do
+            def result
+              Result::Passed.new
+            end
+          end
+
+          Failed = Class.new do
+            def execute(test_step, monitor)
+              test_step.skip
+            end
+
+            def result
+              Result::Failed.new
+            end
+          end
+        end
+
       end
     end
   end
