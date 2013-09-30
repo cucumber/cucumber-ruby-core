a8321812f91ee3b335989ff8b2c376e060c5fd61
Matt Wynne <matt@mattwynne.net>
2013-09-30 22:43:40 +0100

    Remove dead code

--- lib/cucumber/core/ast/background.rb
+++ lib/cucumber/core/ast/background.rb
@@ -24,27 +24,8 @@ module Cucumber
           raw_steps
         end
 
-        def to_sexp
-          sexp = [:background, line, keyword]
-          sexp += [name] unless name.empty?
-          comment = comment.to_sexp
-          sexp += [comment] if comment
-          sexp += steps.to_sexp if steps.any?
-          sexp
-        end
-
-        # Override this method, as there are situations where the background
-        # wind up being the one called fore Before scenarios, and
-        # backgrounds don't have tags.
-        def source_tags
-          []
-        end
-
-        def source_tag_names
-          source_tags.map { |tag| tag.name }
-        end
-
         private
+
         def description_for_visitors
           :background
         end
