42b0820dea861723890fe4d3d562c3d2652b088b
Matt Wynne <matt@mattwynne.net>
2014-03-22 16:13:22 +0100

    Better specs

--- spec/cucumber/core/test/step_spec.rb
+++ spec/cucumber/core/test/step_spec.rb
@@ -13,28 +13,28 @@ module Cucumber::Core::Test
       end
 
       it "describes its source to a visitor" do
-        feature, scenario, step = double, double, double
+        feature, scenario, step_or_hook = double, double, double
         visitor = double
         args = double
-        expect( feature  ).to receive(:describe_to).with(visitor, args)
-        expect( scenario ).to receive(:describe_to).with(visitor, args)
-        expect( step     ).to receive(:describe_to).with(visitor, args)
-        test_step = Step.new([feature, scenario, step])
+        expect( feature      ).to receive(:describe_to).with(visitor, args)
+        expect( scenario     ).to receive(:describe_to).with(visitor, args)
+        expect( step_or_hook ).to receive(:describe_to).with(visitor, args)
+        test_step = Step.new([feature, scenario, step_or_hook])
         test_step.describe_source_to(visitor, args)
       end
     end
 
-    describe "executing a step" do
+    describe "executing" do
       let(:ast_step) { double }
 
-      context "when a passing mapping exists for the step" do
+      context "when a passing mapping exists" do
         it "returns a passing result" do
           test_step = Step.new([ast_step]).with_mapping {}
           expect( test_step.execute ).to be_passed
         end
       end
 
-      context "when a failing mapping exists for the step" do
+      context "when a failing mapping exists" do
         let(:exception) { StandardError.new('oops') }
 
         it "returns a failing result" do
@@ -44,12 +44,20 @@ module Cucumber::Core::Test
           expect( result.exception ).to eq exception
         end
       end
+
+      context "with no mapping" do
+        it "returns an Undefined result" do
+          test_step = Step.new([ast_step])
+          result = test_step.execute
+          expect( result           ).to be_undefined
+        end
+      end
     end
 
-    it "exposes the name and location of the last source node as attributes" do
+    it "exposes the name and location of the AST step or hook as attributes" do
       name, location = double, double
-      ast_step = double(name: name, location: location)
-      test_step = Step.new([ast_step])
+      step_or_hook = double(name: name, location: location)
+      test_step = Step.new([step_or_hook])
       expect( test_step.name     ).to eq name
       expect( test_step.location ).to eq location
     end
