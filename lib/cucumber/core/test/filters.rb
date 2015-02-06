28e1410483388b19088ffa8fdec3ee739090e64d
Matt Wynne <matt@mattwynne.net>
2015-02-06 21:33:45 +0000

    require SortByLocation filter with the rest

--- lib/cucumber/core/test/filters.rb
+++ lib/cucumber/core/test/filters.rb
@@ -1,3 +1,4 @@
 require 'cucumber/core/test/filters/locations_filter'
 require 'cucumber/core/test/filters/name_filter'
 require 'cucumber/core/test/filters/tag_filter'
+require 'cucumber/core/test/filters/sort_by_location'
