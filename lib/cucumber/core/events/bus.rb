c865317f30e2250446f6779bf52651e09aad1346
Matt Wynne <matt@mattwynne.net>
2016-05-06 11:22:31 +0100

    Simplify - insist on using event_id symbols

--- lib/cucumber/core/events/bus.rb
+++ lib/cucumber/core/events/bus.rb
@@ -19,23 +19,22 @@ module Cucumber
         # of the event.
         def on(event_id, handler_object = nil, &handler_proc)
           handler = handler_proc || handler_object
-          raise ArgumentError.new("Please pass either an object or a handler block") unless handler
-          event_class = parse_event_id(event_id)
+          raise ArgumentError, "Please pass either an object or a handler block" unless handler
+          raise ArgumentError, "Please use a symbol for the event_id" unless event_id.is_a?(Symbol)
+          raise ArgumentError, "Event ID #{event_id} is not recognised. Try one of these:\n#{event_types.keys.join("\n")}" unless is_registered_id?(event_id)
+          event_class = event_types[event_id]
           handlers_for(event_class) << handler
-        rescue EventIdError => error
-          raise error, error.message + "\nDid you get the ID of the event wrong? Try one of these:\n#{event_types.keys.join("\n")}", error.backtrace
         end
 
         # Broadcast an event
         def broadcast(event)
-          raise ArgumentError, "Please pass an Event" unless event.is_a?(Event)
-          ensure_registered(event.class)
+          raise ArgumentError, "Event type #{event.class} is not registered. Try one of these:\n#{event_types.values.join("\n")}" unless is_registered_type?(event.class)
           handlers = handlers_for(event.class)
           handlers.each { |handler| handler.call(event) }
         end
 
         def method_missing(event_id, *args)
-          event_class = event_types.fetch(Events::EventId(event_id)) { super }
+          event_class = event_types.fetch(event_id) { super }
           broadcast event_class.new(*args)
         rescue NameError => error
           raise error, error.message + "\nDid you get the ID of the event wrong? Try one of these:\n#{event_types.keys.join("\n")}", error.backtrace
@@ -47,61 +46,12 @@ module Cucumber
           @handlers[event_class.to_s] ||= []
         end
 
-        def parse_event_id(raw)
-          case raw
-          when Class
-            event_type = raw
-            ensure_registered(event_type)
-            event_type
-          else
-            event_id = raw
-            event_types.fetch(event_id) do
-              raise EventIdError.new(event_id)
-            end
-          end
+        def is_registered_id?(event_id)
+          event_types.keys.include?(event_id)
         end
 
-        def ensure_registered(event_type)
-          return if event_types.values.include?(event_type)
-          raise EventTypeError.new(event_type)
-        end
-      end
-
-      def self.EventId(raw)
-        return raw if raw.is_a?(Symbol)
-        EventId.new(raw.name).to_sym
-      end
-
-      # Utility class to help translate back and forth between types and symbols for events
-      class EventId
-        def initialize(type_name)
-          @type_name = type_name
-        end
-
-        def to_sym
-          underscore(@type_name.split("::").last).to_sym
-        end
-
-        private
-
-        def underscore(string)
-          string.to_s.gsub(/::/, '/').
-            gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-            gsub(/([a-z\d])([A-Z])/,'\1_\2').
-            tr("-", "_").
-            downcase
-        end
-      end
-
-      EventIdError = Class.new(StandardError) do
-        def initialize(event_id)
-          super "No Event type with ID `#{event_id}` is registered with the event bus."
-        end
-      end
-
-      EventTypeError = Class.new(StandardError) do
-        def initialize(event_type)
-          super "No Event type `#{event_type}` is registered with the event bus."
+        def is_registered_type?(event_type)
+          event_types.values.include?(event_type)
         end
       end
 
--- spec/cucumber/core/events/bus_spec.rb
+++ spec/cucumber/core/events/bus_spec.rb
@@ -16,9 +16,24 @@ module Cucumber
         let(:registry) { { test_event: TestEvent, another_test_event: AnotherTestEvent } }
 
         context "broadcasting events" do
+
+          it "can broadcast by calling a method named after the event ID" do
+            called = false
+            bus.on(:test_event) {called = true }
+            bus.test_event
+            expect(called).to be true
+          end
+
+          it "can broadcast by calling the `broadcast` method with an instance of the event type" do
+            called = false
+            bus.on(:test_event) {called = true }
+            bus.broadcast(TestEvent.new(:some_attribute))
+            expect(called).to be true
+          end
+
           it "calls a subscriber for an event, passing details of the event" do
             received_payload = nil
-            bus.on(TestEvent) do |event|
+            bus.on :test_event do |event|
               received_payload = event
             end
 
@@ -29,7 +44,7 @@ module Cucumber
 
           it "does not call subscribers for other events" do
             handler_called = false
-            bus.on(TestEvent) do
+            bus.on :test_event do
               handler_called = true
             end
 
@@ -40,10 +55,10 @@ module Cucumber
 
           it "broadcasts to multiple subscribers" do
             received_events = []
-            bus.on(TestEvent) do
+            bus.on :test_event do
               received_events << :event
             end
-            bus.on(TestEvent) do
+            bus.on :test_event do
               received_events << :event
             end
 
@@ -57,12 +72,6 @@ module Cucumber
           end
 
           context "#broadcast method" do
-            it "must be passed an instance of Event" do
-              expect { 
-                bus.broadcast Object.new
-              }.to raise_error(ArgumentError)
-            end
-
             it "must be passed an instance of a registered event type" do
               expect { 
                 bus.broadcast UnregisteredEvent
@@ -87,24 +96,7 @@ module Cucumber
           it "raises an error if you use an unknown Event ID" do
             expect { 
               bus.on(:some_unknown_event) { :whatever }
-            }.to raise_error(EventIdError)
-          end
-
-          it "allows subscription by class" do
-            received_payload = nil
-            bus.on(TestEvent) do |event|
-              received_payload = event
-            end
-
-            bus.test_event :some_attribute
-
-            expect(received_payload.some_attribute).to eq(:some_attribute)
-          end
-
-          it "raises an error if you use an un-registered Event type" do
-            expect { 
-              bus.on(UnregisteredEvent) { :whatever }
-            }.to raise_error(EventTypeError)
+            }.to raise_error(ArgumentError)
           end
 
           it "allows handlers that are objects with a `call` method" do
