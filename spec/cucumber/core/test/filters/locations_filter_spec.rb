eca529959dc113bfba4a5e8f910e574ccb9f62c8
Micah Geisel <micah@botandrose.com>
2017-08-18 10:50:16 -0700

    Fix DataTable's Location to be aware of all of its lines.

--- spec/cucumber/core/test/filters/locations_filter_spec.rb
+++ spec/cucumber/core/test/filters/locations_filter_spec.rb
@@ -96,6 +96,8 @@ module Cucumber::Core
                 Given a table
                   | a | b |
                   | 1 | 2 |
+                  | 3 | 4 |
+
           END
         end
 
@@ -192,9 +194,24 @@ module Cucumber::Core
             test_cases.find { |c| c.name == 'with a table' }
           end
 
-          it "matches a location on the first table row" do
+          it "matches a location at the start of the table" do
             location = Ast::Location.new(file, 23)
             expect( test_case.match_locations?([location]) ).to be_truthy
+          end
+
+          it "matches a location in the middle of the table" do
+            location = Ast::Location.new(file, 24)
+            expect( test_case.match_locations?([location]) ).to be_truthy
+          end
+
+          it "matches a location at the end of the table" do
+            location = Ast::Location.new(file, 25)
+            expect( test_case.match_locations?([location]) ).to be_truthy
+          end
+
+          it "does not match a location after the table" do
+            location = Ast::Location.new(file, 26)
+            expect( test_case.match_locations?([location]) ).to be_falsey
           end
         end
 
