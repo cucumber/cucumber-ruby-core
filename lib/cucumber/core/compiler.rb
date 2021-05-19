cb6a0f9f82faba13d2e53eb32ef4799eb315555b
aurelien-reeves <aurelien.reeves@smartbear.com>
2021-05-19 14:45:29 +0200

    Fix specs

--- lib/cucumber/core/compiler.rb
+++ lib/cucumber/core/compiler.rb
@@ -35,11 +35,11 @@ module Cucumber
       private
 
       def create_test_case(pickle)
-        uri = pickle.uri
-        test_steps = pickle.steps.map { |step| create_test_step(step, uri) }
+        uri = pickle[:uri]
+        test_steps = pickle[:steps].map { |step| create_test_step(step, uri) }
         lines = source_lines_for_pickle(pickle).sort.reverse
-        tags = pickle.tags.map { |tag| Test::Tag.new(Test::Location.new(uri, source_line_for_pickle_tag(tag)), tag.name) }
-        test_case = Test::Case.new(id_generator.new_id, pickle.name, test_steps, Test::Location.new(uri, lines), tags, pickle.language)
+        tags = pickle[:tags].map { |tag| Test::Tag.new(Test::Location.new(uri, source_line_for_pickle_tag(tag)), tag[:name]) }
+        test_case = Test::Case.new(id_generator.new_id, pickle[:name], test_steps, Test::Location.new(uri, lines), tags, pickle[:language])
         @event_bus&.test_case_created(test_case, pickle)
         test_case
       end
@@ -47,23 +47,23 @@ module Cucumber
       def create_test_step(pickle_step, uri)
         lines = source_lines_for_pickle_step(pickle_step).sort.reverse
         multiline_arg = create_multiline_arg(pickle_step, uri)
-        step = Test::Step.new(id_generator.new_id, pickle_step.text, Test::Location.new(uri, lines), multiline_arg)
+        step = Test::Step.new(id_generator.new_id, pickle_step[:text], Test::Location.new(uri, lines), multiline_arg)
         @event_bus&.test_step_created(step, pickle_step)
         step
       end
 
       def create_multiline_arg(pickle_step, _uri)
-        if pickle_step.argument
-          if pickle_step.argument.doc_string
-            doc_string = pickle_step.argument.doc_string
+        if pickle_step[:argument]
+          if pickle_step[:argument][:docString]
+            doc_string = pickle_step[:argument][:docString]
             Test::DocString.new(
-              doc_string.content,
-              doc_string.media_type
+              doc_string[:content],
+              doc_string[:media_type]
             )
-          elsif pickle_step.argument.data_table
-            data_table = pickle_step.argument.data_table
+          elsif pickle_step[:argument][:dataTable]
+            data_table = pickle_step[:argument][:dataTable]
             Test::DataTable.new(
-              data_table.rows.map { |row| row.cells.map { |cell| cell.value } }
+              data_table[:rows].map { |row| row[:cells].map { |cell| cell[:value] } }
             )
           end
         else
@@ -72,19 +72,19 @@ module Cucumber
       end
 
       def source_lines_for_pickle(pickle)
-        pickle.ast_node_ids.map { |id| source_line(id) }
+        pickle[:astNodeIds].map { |id| source_line(id) }
       end
 
       def source_lines_for_pickle_step(pickle_step)
-        pickle_step.ast_node_ids.map { |id| source_line(id) }
+        pickle_step[:astNodeIds].map { |id| source_line(id) }
       end
 
       def source_line_for_pickle_tag(tag)
-        source_line(tag.ast_node_id)
+        source_line(tag[:astNodeId])
       end
 
       def source_line(id)
-        gherkin_query.location(id).line
+        gherkin_query.location(id)[:line]
       end
     end
   end
