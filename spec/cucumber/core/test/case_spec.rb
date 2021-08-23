168c35bdb04913a60dc9a9e4270e6d72cb21ba4c
aurelien-reeves <aurelien.reeves@hiptest.net>
2021-08-23 16:51:30 +0200

    Remove useless magic comment
    This should prevent rubocop to fail because of an offsense.
    Also, UTF-8 is the default since ruby 2.0. I guess jRuby has been
    updated too.

--- spec/cucumber/core/test/case_spec.rb
+++ spec/cucumber/core/test/case_spec.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-# -*- coding: utf-8 -*-
 require 'cucumber/core'
 require 'cucumber/core/gherkin/writer'
 require 'cucumber/core/platform'
@@ -25,7 +24,7 @@ module Cucumber
           it "describes itself to a visitor" do
             visitor = double
             args = double
-            expect( visitor ).to receive(:test_case).with(test_case, args)
+            expect(visitor).to receive(:test_case).with(test_case, args)
             test_case.describe_to(visitor, args)
           end
 
@@ -33,68 +32,68 @@ module Cucumber
             visitor = double
             args = double
             test_steps.each do |test_step|
-              expect( test_step ).to receive(:describe_to).with(visitor, args)
+              expect(test_step).to receive(:describe_to).with(visitor, args)
             end
-            allow( visitor ).to receive(:test_case).and_yield(visitor)
+            allow(visitor).to receive(:test_case).and_yield(visitor)
             test_case.describe_to(visitor, args)
           end
 
           it "describes around hooks in order" do
             visitor = double
-            allow( visitor ).to receive(:test_case).and_yield(visitor)
-            first_hook, second_hook = double, double
-            expect( first_hook ).to receive(:describe_to).ordered.and_yield
-            expect( second_hook ).to receive(:describe_to).ordered.and_yield
+            allow(visitor).to receive(:test_case).and_yield(visitor)
+            first_hook = double
+            second_hook = double
+            expect(first_hook).to receive(:describe_to).ordered.and_yield
+            expect(second_hook).to receive(:describe_to).ordered.and_yield
             around_hooks = [first_hook, second_hook]
             Test::Case.new(id, name, [], location, tags, language, around_hooks).describe_to(visitor, double)
           end
-
         end
 
         describe "#name" do
           it "the name is passed when creating the test case" do
-            expect( test_case.name ).to eq(name)
+            expect(test_case.name).to eq(name)
           end
         end
 
         describe "#location" do
           it "the location is passed when creating the test case" do
-            expect( test_case.location ).to eq(location)
+            expect(test_case.location).to eq(location)
           end
         end
 
         describe "#tags" do
           it "the tags are passed when creating the test case" do
-            expect( test_case.tags ).to eq(tags)
+            expect(test_case.tags).to eq(tags)
           end
         end
 
         describe "matching tags" do
           let(:tags) { ['@a', '@b', '@c'].map { |value| Tag.new(location, value) } }
           it "matches tags using tag expressions" do
-            expect( test_case.match_tags?(['@a and @b']) ).to be_truthy
-            expect( test_case.match_tags?(['@a or @d']) ).to be_truthy
-            expect( test_case.match_tags?(['not @d']) ).to be_truthy
-            expect( test_case.match_tags?(['@a and @d']) ).to be_falsy
+            expect(test_case.match_tags?(['@a and @b'])).to be_truthy
+            expect(test_case.match_tags?(['@a or @d'])).to be_truthy
+            expect(test_case.match_tags?(['not @d'])).to be_truthy
+            expect(test_case.match_tags?(['@a and @d'])).to be_falsy
           end
 
           it "matches handles multiple expressions" do
-            expect( test_case.match_tags?(['@a and @b', 'not @d']) ).to be_truthy
-            expect( test_case.match_tags?(['@a and @b', 'not @c']) ).to be_falsy
+            expect(test_case.match_tags?(['@a and @b', 'not @d'])).to be_truthy
+            expect(test_case.match_tags?(['@a and @b', 'not @c'])).to be_falsy
           end
         end
 
         describe "matching names" do
           let(:name) { 'scenario' }
           it "matches names against regexp" do
-            expect( test_case.match_name?(/scenario/) ).to be_truthy
+            expect(test_case.match_name?(/scenario/)).to be_truthy
           end
         end
 
         describe "#language" do
           let(:language) { 'en-pirate' }
           it "the language is passed when creating the test case" do
-            expect( test_case.language ).to eq 'en-pirate'
+            expect(test_case.language).to eq 'en-pirate'
           end
         end
 
@@ -120,7 +119,6 @@ module Cucumber
             expect(test_case_instances[0]).not_to equal test_case_instances[1]
           end
         end
-
       end
     end
   end
