9e133497b45c5acef9cfdf2471956a9393c05332
Kimmie <kimthu.bui@employmenthero.com>
2023-06-06 13:23:08 +0700

    Fix retried scenarios being run after another scenario

--- lib/cucumber/core/report/summary.rb
+++ lib/cucumber/core/report/summary.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
+
+require 'set'
+
 module Cucumber
   module Core
     module Report
@@ -6,7 +9,7 @@ module Cucumber
         attr_reader :test_cases, :test_steps
 
         def initialize(event_bus)
-          @previous_test_case = nil
+          @previous_test_cases = Set.new
           @test_cases = Test::Result::Summary.new
           @test_steps = Test::Result::Summary.new
           subscribe_to(event_bus)
@@ -20,8 +23,8 @@ module Cucumber
 
         def subscribe_to(event_bus)
           event_bus.on(:test_case_finished) do |event|
-            if event.test_case != @previous_test_case
-              @previous_test_case = event.test_case
+            if !@previous_test_cases.include?(event.test_case)
+              @previous_test_cases << event.test_case
               event.result.describe_to test_cases
             elsif event.result.passed? || event.result.skipped?
               test_cases.flaky
--- spec/cucumber/core/report/summary_spec.rb
+++ spec/cucumber/core/report/summary_spec.rb
@@ -56,25 +56,62 @@ module Cucumber::Core::Report
         expect( @summary.test_cases.total ).to eq(1)
       end
 
-      it "handles flaky test cases" do
-        allow(test_case).to receive(:==).and_return(false, true)
-        event_bus.send(:test_case_finished, test_case, failed_result)
-        event_bus.send(:test_case_finished, test_case, passed_result)
+      context 'handles flaky tests cases' do
+        context 'with following skip test cases' do
+          it do
+            allow(test_case).to receive(:==).and_return(false, true)
+            event_bus.send(:test_case_finished, test_case, failed_result)
+            event_bus.send(:test_case_finished, test_case, skipped_result)
 
-        expect( @summary.test_cases.total(:failed) ).to eq(0)
-        expect( @summary.test_cases.total(:flaky) ).to eq(1)
-        expect( @summary.test_cases.total ).to eq(1)
-      end
+            expect( @summary.test_cases.total(:failed) ).to eq(0)
+            expect( @summary.test_cases.total(:skipped) ).to eq(0)
+            expect( @summary.test_cases.total(:flaky) ).to eq(1)
+            expect( @summary.test_cases.total ).to eq(1)
+          end
+        end
 
-      it "handless flaky with following skip test cases" do
-        allow(test_case).to receive(:==).and_return(false, true)
-        event_bus.send(:test_case_finished, test_case, failed_result)
-        event_bus.send(:test_case_finished, test_case, skipped_result)
+        context 'with following passed test cases' do
+          it do
+            event_bus.send(:test_case_finished, test_case, failed_result)
+            event_bus.send(:test_case_finished, test_case, passed_result)
 
-        expect( @summary.test_cases.total(:failed) ).to eq(0)
-        expect( @summary.test_cases.total(:skipped) ).to eq(0)
-        expect( @summary.test_cases.total(:flaky) ).to eq(1)
-        expect( @summary.test_cases.total ).to eq(1)
+            expect( @summary.test_cases.total(:passed) ).to eq(0)
+            expect( @summary.test_cases.total(:failed) ).to eq(0)
+            expect( @summary.test_cases.total(:flaky) ).to eq(1)
+            expect( @summary.test_cases.total ).to eq(1)
+          end
+        end
+
+        context 'with another passed test case and then following the retried test cases' do
+          context 'passed retried test cases' do
+            it do
+              event_bus.send(:test_case_finished, test_case, failed_result)
+              event_bus.send(:test_case_finished, double, passed_result)
+              event_bus.send(:test_case_finished, test_case, failed_result)
+              event_bus.send(:test_case_finished, test_case, passed_result)
+
+              expect( @summary.test_cases.total(:passed) ).to eq(1)
+              expect( @summary.test_cases.total(:failed) ).to eq(0)
+              expect( @summary.test_cases.total(:flaky) ).to eq(1)
+              expect( @summary.test_cases.total ).to eq(2)
+            end
+          end
+
+          context 'failed retried test cases' do
+            it do
+              event_bus.send(:test_case_finished, test_case, failed_result)
+              event_bus.send(:test_case_finished, double, passed_result)
+              event_bus.send(:test_case_finished, test_case, failed_result)
+              event_bus.send(:test_case_finished, test_case, failed_result)
+              event_bus.send(:test_case_finished, test_case, failed_result)
+
+              expect( @summary.test_cases.total(:passed) ).to eq(1)
+              expect( @summary.test_cases.total(:failed) ).to eq(1)
+              expect( @summary.test_cases.total(:flaky) ).to eq(0)
+              expect( @summary.test_cases.total ).to eq(2)
+            end
+          end
+        end
       end
     end
 
