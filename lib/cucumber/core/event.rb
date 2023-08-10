95ab669aa6c9224eaebfc1b3ecb7e56b4a5d5c33
M.P. Korstanje <rien.korstanje@gmail.com>
2023-08-11 01:48:17 +0200

    Fix rubocop validations

--- lib/cucumber/core/event.rb
+++ lib/cucumber/core/event.rb
@@ -47,7 +47,7 @@ module Cucumber
         private
 
         def underscore(string)
-          string.to_s.gsub(/::/, '/').
+          string.to_s.gsub('::', '/').
           gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').
           gsub(/([a-z\d])([A-Z])/, '\1_\2').
           tr("-", "_").
