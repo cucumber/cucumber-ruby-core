5234d76e9701a4e59aff15f000ab189fba58d673
Luke Hill <lukehill_uk@hotmail.com>
2023-09-08 12:00:18 +0100

    Improved AbcScore for Report::Summary

--- lib/cucumber/core/report/summary.rb
+++ lib/cucumber/core/report/summary.rb
@@ -20,19 +20,27 @@ module Cucumber
         private
 
         def subscribe_to(event_bus)
+          register_test_case_finished_listener(event_bus)
+          register_test_step_finished_listener(event_bus)
+          self
+        end
+
+        def register_test_case_finished_listener(event_bus)
           event_bus.on(:test_case_finished) do |event|
             if event.test_case != @previous_test_case
               @previous_test_case = event.test_case
-              event.result.describe_to test_cases
+              event.result.describe_to(test_cases)
             elsif event.result.passed? || event.result.skipped?
               test_cases.flaky
               test_cases.decrement_failed
             end
           end
+        end
+
+        def register_test_step_finished_listener(event_bus)
           event_bus.on(:test_step_finished) do |event|
-            event.result.describe_to test_steps unless event.test_step.hook?
+            event.result.describe_to(test_steps) unless event.test_step.hook?
           end
-          self
         end
       end
     end
