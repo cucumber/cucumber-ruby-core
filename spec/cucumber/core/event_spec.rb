b9ffa9b8456f2d698301d6c9f516e51865960150
Luke Hill <lukehill_uk@hotmail.com>
2023-09-08 10:26:46 +0100

    Manual fixes for Lint/ConstantDefinitionInBlock for event_spec

--- spec/cucumber/core/event_spec.rb
+++ spec/cucumber/core/event_spec.rb
@@ -2,39 +2,35 @@
 
 require 'cucumber/core/event'
 
-module Cucumber
-  module Core
-    describe Event do
-      describe '.new' do
-        it 'generates new types of events' do
-          my_event_type = described_class.new
-          my_event = my_event_type.new
-          expect(my_event).to be_kind_of(described_class)
-        end
+describe Cucumber::Core::Event do
+  describe '.new' do
+    it 'generates new types of events' do
+      my_event_type = described_class.new
+      my_event = my_event_type.new
+      expect(my_event).to be_kind_of(described_class)
+    end
 
-        it 'generates events with attributes' do
-          my_event_type = described_class.new(:foo, :bar)
-          my_event = my_event_type.new(1, 2)
-          expect(my_event.attributes).to eq [1, 2]
-          expect(my_event.foo).to eq 1
-          expect(my_event.bar).to eq 2
-        end
-      end
+    it 'generates events with attributes' do
+      my_event_type = described_class.new(:foo, :bar)
+      my_event = my_event_type.new(1, 2)
+      expect(my_event.attributes).to eq [1, 2]
+      expect(my_event.foo).to eq 1
+      expect(my_event.bar).to eq 2
+    end
+  end
 
-      describe 'a generated event' do
-        class MyEventType < Event.new(:foo, :bar)
-        end
+  describe 'a generated event' do
+    let(:my_event_type) do
+      Class.new(Event.new(:foo, :bar))
+    end
 
-        it 'can be converted to a hash' do
-          my_event = MyEventType.new(1, 2)
-          expect(my_event.to_h).to eq foo: 1, bar: 2
-        end
+    it 'can be converted to a hash' do
+      expect(my_event_type.new(1, 2).to_h).to eq(foo: 1, bar: 2)
+    end
 
-        it 'has an event_id' do
-          expect(MyEventType.event_id).to eq :my_event_type
-          expect(MyEventType.new(1, 2).event_id).to eq :my_event_type
-        end
-      end
+    it 'has an event_id' do
+      expect(my_event_type.event_id).to eq(:my_event_type)
+      expect(my_event_type.new(1, 2).event_id).to eq(:my_event_type)
     end
   end
 end
