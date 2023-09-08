145df743cb01608e0200e5c2542041559b268ba1
Luke Hill <lukehill_uk@hotmail.com>
2023-09-08 17:19:37 +0100

    Fully fix event spec

--- spec/cucumber/core/event_spec.rb
+++ spec/cucumber/core/event_spec.rb
@@ -20,17 +20,18 @@ describe Cucumber::Core::Event do
   end
 
   describe 'a generated event' do
-    let(:my_event_type) do
-      Class.new(described_class.new(:foo, :bar))
+    before do
+      # Needed to be set this way because of a name inflection in the method
+      Object.const_set('MyEventType', Class.new(described_class.new(:foo, :bar)))
     end
 
     it 'can be converted to a hash' do
-      expect(my_event_type.new(1, 2).to_h).to eq(foo: 1, bar: 2)
+      expect(MyEventType.new(1, 2).to_h).to eq(foo: 1, bar: 2)
     end
 
     it 'has an event_id' do
-      expect(my_event_type.event_id).to eq(:my_event_type)
-      expect(my_event_type.new(1, 2).event_id).to eq(:my_event_type)
+      expect(MyEventType.event_id).to eq(:my_event_type)
+      expect(MyEventType.new(1, 2).event_id).to eq(:my_event_type)
     end
   end
 end
