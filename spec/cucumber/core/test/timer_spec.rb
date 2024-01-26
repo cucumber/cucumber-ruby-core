5167a88aa05bc608eba903e918c7d3a7be48d9b4
Luke Hill <lukehill_uk@hotmail.com>
2024-01-26 12:10:14 +0000

    Tidy up timer spec

--- spec/cucumber/core/test/timer_spec.rb
+++ spec/cucumber/core/test/timer_spec.rb
@@ -3,24 +3,17 @@
 require 'cucumber/core/test/timer'
 require 'cucumber/core/test/duration_matcher'
 
-module Cucumber
-  module Core
-    module Test
-      describe Timer do
-        before do
-          allow(Timer::MonotonicTime).to receive(:time_in_nanoseconds)
-                                            .and_return(525_702_744_080_000, 525_702_744_080_001)
-        end
+describe Cucumber::Core::Test::Timer do
+  let(:random_time) { 525_702_744_080_000 }
+  let(:other_random_time) { 525_702_744_080_001 }
 
-        it 'returns a Result::Duration object' do
-          timer = described_class.new.start
-          expect(timer.duration).to be_duration 1
-        end
+  before do
+    allow(Cucumber::Core::Test::Timer::MonotonicTime).to receive(:time_in_nanoseconds).and_return(random_time, other_random_time)
+  end
 
-        it 'would be slow to test' do
-          # so we won't
-        end
-      end
-    end
+  it 'returns a Result::Duration object' do
+    timer = described_class.new.start
+
+    expect(timer.duration).to be_duration(1)
   end
 end
