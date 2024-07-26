7bf1b7f55315a618b57cdafc283165e38a2b6193
Luke Hill <lukehill_uk@hotmail.com>
2024-07-26 08:29:47 +0100

    Test default multiline arg behaviour in shared steps

--- spec/support/shared_context/steps.rb
+++ spec/support/shared_context/steps.rb
@@ -1,9 +1,9 @@
 require 'cucumber/core/test/step'
 
 RSpec.shared_context "steps" do
-  let(:passing)          { Cucumber::Core::Test::Step.new(1, 'Passing Step', double, double).with_action { :no_op } }
-  let(:failing)          { Cucumber::Core::Test::Step.new(2, 'Failing Step', double, double).with_action { raise StandardError, 'Error' } }
-  let(:pending)          { Cucumber::Core::Test::Step.new(3, 'Pending Step', double, double).with_action { raise Cucumber::Core::Test::Result::Pending, 'TODO' } }
-  let(:skipping)         { Cucumber::Core::Test::Step.new(4, 'Skipped Step', double, double).with_action { raise Cucumber::Core::Test::Result::Skipped } }
-  let(:undefined)        { Cucumber::Core::Test::Step.new(5, 'Undefined Step', double, double) }
+  let(:passing)          { Cucumber::Core::Test::Step.new(1, 'Passing Step', double).with_action { :no_op } }
+  let(:failing)          { Cucumber::Core::Test::Step.new(2, 'Failing Step', double).with_action { raise StandardError, 'Error' } }
+  let(:pending)          { Cucumber::Core::Test::Step.new(3, 'Pending Step', double).with_action { raise Cucumber::Core::Test::Result::Pending, 'TODO' } }
+  let(:skipping)         { Cucumber::Core::Test::Step.new(4, 'Skipped Step', double).with_action { raise Cucumber::Core::Test::Result::Skipped } }
+  let(:undefined)        { Cucumber::Core::Test::Step.new(5, 'Undefined Step', double) }
 end
