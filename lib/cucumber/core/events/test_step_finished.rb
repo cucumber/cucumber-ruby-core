147fe9225015cbb9d16eb774cd9803d2686ca0e3
Luke Hill <lukehill_uk@hotmail.com>
2026-04-15 19:35:06 +0100

    Remove duplicate reader logic

--- lib/cucumber/core/events/test_step_finished.rb
+++ lib/cucumber/core/events/test_step_finished.rb
@@ -8,10 +8,7 @@ module Cucumber
       # Signals that a {Test::Step} has finished executing
       class TestStepFinished < Event.new(:test_step, :result)
         # @return [Test::Step] the test step that was executed
-        attr_reader :test_step
-
         # @return [Test::Result] the result of running the {Test::Step}
-        attr_reader :result
       end
     end
   end
--- lib/cucumber/core/events/test_step_started.rb
+++ lib/cucumber/core/events/test_step_started.rb
@@ -8,7 +8,6 @@ module Cucumber
       # Signals that a {Test::Step} is about to be executed
       class TestStepStarted < Event.new(:test_step)
         # @return [Test::Step] the test step to be executed
-        attr_reader :test_step
       end
     end
   end
